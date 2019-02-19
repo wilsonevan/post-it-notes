a67643d3dde9d2390cc69c2ef6590406428358b1
Evan Wilson <wilsonevan100@gmail.com>
2019-02-18 21:19:04 -0700

    Basic Obj 1-6

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,51 @@
+class NotesController < ApplicationController
+def index
+    @notes = Note.all
+  end
+  
+  def show
+    @note = Note.find(params[:id])
+  end
+
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(notes_params)
+
+    if @note.save
+      redirect_to notes_path
+    else
+      render :new
+    end
+      
+  end
+
+  def edit
+    @note = Note.find(params[:id])
+  end
+  
+  def update
+    @note = Note.find(params[:id])
+
+    if @note.update(notes_params)
+      redirect_to notes_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    # Note.destroy(params[:id])
+    # puts Note.find(params[:id])
+    Note.find(params[:id]).destroy
+    redirect_to notes_path
+  end
+
+private
+
+  def notes_params
+    params.require(:note).permit(:title, :author, :body)
+  end
+end
---
+++ test/controllers/notes_controller_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class NotesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get notes_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get notes_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get notes_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get notes_create_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get notes_update_url
+    assert_response :success
+  end
+
+  test "should get delete" do
+    get notes_delete_url
+    assert_response :success
+  end
+
+end
